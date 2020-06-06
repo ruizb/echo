150186816ece358f3ce09bc0da2ac23581fe635e
Benoit Ruiz <benoit.ruiz01@gmail.com>
2020-06-06 21:02:01 +0200

    Set the parts 1, 2, 3 with some interaction

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,52 @@
+import audioUrl from './sounds/ref-sound-sch-250Hz.wav'
+
 document.getElementById('start-experiment')?.addEventListener('click', () => {
-  alert(`Let's go!`)
+  document.getElementById('part-1')?.classList.add('hide')
+  document.getElementById('part-2')?.classList.remove('hide')
 })
+
+document.getElementById('configure-sound')?.addEventListener('click', (evt) => {
+  evt.preventDefault()
+  document.getElementById('part-2')?.classList.add('hide')
+  document.getElementById('part-3')?.classList.remove('hide')
+})
+
+document.getElementById('start-tests')?.addEventListener('click', () => {
+  document.getElementById('part-3')?.classList.add('hide')
+  document.getElementById('part-4')?.classList.remove('hide')
+})
+
+$('i.icon.info').popup()
+
+document
+  .querySelectorAll('input[type="radio"][name="sounds-reactions"]')
+  .forEach((radioBtn) =>
+    radioBtn.addEventListener('change', (evt) => {
+      const radioBtnIsYes = (evt.target as HTMLInputElement).value === 'yes'
+      document
+        .getElementById('sounds-reactions-list-field')
+        ?.classList.remove(radioBtnIsYes ? 'hide' : 'show')
+      document
+        .getElementById('sounds-reactions-list-field')
+        ?.classList.add(radioBtnIsYes ? 'show' : 'hide')
+    })
+  )
+
+const audio = new Audio(audioUrl)
+audio.volume = 0.1
+audio.addEventListener('ended', () =>
+  document.getElementById('play-ref-sound')?.classList.remove('disabled')
+)
+
+document
+  .getElementById('play-ref-sound')
+  ?.addEventListener('click', ({ target }) => {
+    ;(target as HTMLButtonElement).classList.add('disabled')
+    audio.play()
+  })
+
+document
+  .getElementById('ref-sound-slider')
+  ?.addEventListener('change', ({ target }) => {
+    audio.volume = parseInt((target as HTMLInputElement).value, 10) / 100
+  })
