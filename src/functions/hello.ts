05ae030f746c1c2375990f3d2ac554c745be8b87
Benoit Ruiz <benoit.ruiz01@gmail.com>
2020-06-10 11:48:03 +0200

    Improve function

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,24 +1,65 @@
 import { Context } from 'aws-lambda'
 import * as sgMail from '@sendgrid/mail'
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
+const {
+  SENDGRID_API_KEY,
+  SENDGRID_FROM_EMAIL,
+  SENDGRID_FROM_NAME,
+  SENDGRID_TO_EMAIL
+} = process.env
+
+const sendEmail = (
+  client: typeof sgMail,
+  {
+    senderEmail,
+    senderName,
+    receiverEmail,
+    subject,
+    message,
+    htmlMessage
+  }: {
+    senderEmail: string
+    receiverEmail: string
+    subject: string
+    message: string
+    senderName: string
+    htmlMessage: string
+  }
+) =>
+  new Promise((resolve, reject) => {
+    console.log('Sending the email...')
+
+    const data = {
+      from: { email: senderEmail, name: senderName },
+      subject,
+      to: receiverEmail,
+      text: message,
+      html: htmlMessage
+    }
+
+    client
+      .send(data)
+      .then(([response, body]) => {
+        resolve(response)
+      })
+      .catch(reject)
+  })
 
 export async function handler(event: any, context: Context) {
   console.log('context', context)
   console.log('event', event)
 
+  sgMail.setApiKey(SENDGRID_API_KEY!)
+
   if (event.httpMethod === 'POST' && !!event.body) {
-    if (process.env.SENDGRID_FROM_EMAIL && process.env.SENDGRID_TO_EMAIL) {
-      sgMail
-        .send({
-          from: process.env.SENDGRID_FROM_EMAIL,
-          to: process.env.SENDGRID_TO_EMAIL,
-          subject: `Résultats de l'expérience sur l'audition en ligne`,
-          text: event.body,
-          html: `<strong>${event.body}</strong>`
-        })
-        .then(console.log, console.error)
-    }
+    sendEmail(sgMail, {
+      senderName: SENDGRID_FROM_NAME!,
+      senderEmail: SENDGRID_FROM_EMAIL!,
+      receiverEmail: SENDGRID_TO_EMAIL!,
+      subject: `Résultats de l'expérience sur l'audition en ligne`,
+      message: event.body,
+      htmlMessage: `<strong>${event.body}</strong>`
+    }).then(console.log, console.error)
   }
 
   return {
