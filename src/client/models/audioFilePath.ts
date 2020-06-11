7e11912621178606fbbfc63db72e085ecc72324c
Benoit Ruiz <benoit.ruiz01@gmail.com>
2020-06-11 20:02:06 +0200

    Add more sounds for the experiment

--- src/client/models/audioFilePath.ts
+++ src/client/models/audioFilePath.ts
@@ -1,42 +1,65 @@
 import refSoundSch250Hz from '../sounds/ref-sound-sch-250Hz.wav'
 
+import birds from '../sounds/Birds_1.wav'
 import blowingNose from '../sounds/Blowing_nose1.wav'
 import boire from '../sounds/Boire.wav'
 import breathRunning from '../sounds/Breath_running.wav'
+import clapping from '../sounds/Clapping_3.wav'
 import clavier from '../sounds/Clavier.wav'
 import cough from '../sounds/Cough.wav'
+import distortedGuitarNeckDissonance from '../sounds/Distorted Guitar Neck Dissonance_1.wav'
 import eat from '../sounds/Eat.wav'
 import eat3 from '../sounds/Eat_3.wav'
+import femaleScream from '../sounds/Female Scream_2.wav'
+import fingernailsOnChalkboard from '../sounds/Fingernails On Chalkboard_2.wav'
+import forkScratchPlate from '../sounds/Fork Scratch Plate_2.wav'
 import garglingWater from '../sounds/Gargling-Water.wav'
 import hardBreathing from '../sounds/Hard-Breathing.wav'
+import harp from '../sounds/Harp_1.wav'
+import knifeHitGlass from '../sounds/Knife Hit Glass_1.wav'
+import lake from '../sounds/Lake_2.wav'
+import laugh from '../sounds/Laugh_1.wav'
 import penClick from '../sounds/Pen-Click.wav'
 import sniffing from '../sounds/Sniffing.wav'
 import snoringSleepApnea from '../sounds/Snoring_sleepApnea.wav'
 import swallowing from '../sounds/Swallowing.wav'
-import throatClear from '../sounds/Throat_Clear.mp3'
+import throatClear from '../sounds/Throat_Clear.wav'
 import vomit from '../sounds/Vomit.wav'
 import wheezing from '../sounds/Wheezing.wav'
+import whiteNoise from '../sounds/White Noise_1.wav'
+
 import { head } from '../utils'
 
 export const refSoundFilePath = refSoundSch250Hz
 
 const audioFilePaths = [
+  birds,
   blowingNose,
   boire,
   breathRunning,
+  clapping,
   clavier,
   cough,
+  distortedGuitarNeckDissonance,
   eat,
   eat3,
+  femaleScream,
+  fingernailsOnChalkboard,
+  forkScratchPlate,
   garglingWater,
   hardBreathing,
+  harp,
+  knifeHitGlass,
+  lake,
+  laugh,
   penClick,
   sniffing,
   snoringSleepApnea,
   swallowing,
   throatClear,
   vomit,
-  wheezing
+  wheezing,
+  whiteNoise
 ]
 
 export const getFilePathFromName = (
