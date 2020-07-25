ceefa08f84a8814bb9abe427ef40995d8d05aece
Benoit Ruiz <benoit.ruiz01@gmail.com>
2020-07-25 15:31:32 +0200

    Add translations files for EN and FR languages

---
+++ src/client/i18n/en.ts
@@ -0,0 +1,97 @@
+const translations = {
+  homepage_title: `Online experiment about the hearing`,
+  homepage_description: `<p>Hello,</p>
+      <p>Vous vous apprêtez à commencer une expérience en ligne sur l'audition organisée par Arnaud Norena, directeur de recherche au CNRS, laboratoire de Neurosciences Sensorielles et Cognitives, Marseille. Pour toutes informations complémentaires, vous pouvez contacter Arnaud par <a class="ui label" href="mailto:arnaud.norena@univ-amu.fr"><i class="mail icon"></i> email</a>.</p>
+      <p><strong>Avant toute chose, nous souhaitons vous remercier de votre participation à cette recherche !</strong></p>
+      <p>Aucune information identifiante ne vous sera demandée. Assurez-vous de réaliser l'expérience dans de bonnes conditions, c'est-à-dire d'avoir le temps nécessaire pour aller jusqu'au bout (20 minutes environ), être motivé et en bonne forme, et vous trouver dans un endroit calme.</p>
+      <p>Le test consiste à évaluer des sons selon une échelle visuelle allant de très plaisant à très déplaisant :</p>
+      <ul>
+        <li>Si le son présenté est celui d'un aboiement de chien et que vous trouvez ce son très désagréable, alors vous devez cliquer à l'extrémité droite de l'échelle.</li>
+        <li>Si le son présenté est celui du vent dans la forêt et que vous trouvez ce son très agréable, alors vous devez cliquer à l'extrême gauche de l'échelle.</li>
+        <li>Si le son présenté est neutre, vous devez cliquer au milieu de l'échelle.</li>
+      </ul>
+      <p>En général, les sons sont plus ou moins agréables ou désagréables, donc vous devez utiliser toute la longueur de l'échelle pour donner votre réponse. Tous les sons seront présentés à un niveau sonore confortable.</p>`,
+  homepage_startExperiment: `Start the experiment`,
+  userInfo_description: `Cette page consiste à vous demander des informations sur vous et votre audition.`,
+  userInfo_ageLabel: `How old are you?`,
+  userInfoListening_deviceLabel: `Qu'utilisez-vous pour écouter les sons de cette expérience ?`,
+  userInfoListening_deviceHeadset: `Heaset`,
+  userInfoListening_deviceEarphones: `Ear phones`,
+  userInfoListening_deviceSpeakers: `Speakers`,
+  userInfoListening_hearingIssuesLabel: `Selon vous, avez-vous des problèmes d'audition ? <i data-content="Faites-vous répéter souvent, avez-vous des problèmes pour comprendre la parole dans le bruit, etc." class="info circle icon"></i>`,
+  common_yes: `Yes`,
+  common_no: `No`,
+  common_dontKnow: `I don't know`,
+  userInfoListening_tinnitusLabel: `Avez-vous des acouphènes ? <i data-content="Sifflement d'oreille." class="info circle icon"></i>`,
+  userInfo_hypersensibilityLabel: `Avez-vous une hypersensibilité auditive ? <i data-content="Certains sons sont-ils forts ou douloureux à des intensités modestes qui ne provoquent aucune réaction chez les autres ?" class="info circle icon"></i>`,
+  userInfo_hypersensibilityImpactLabel: `A quel point êtes-vous handicapé par cette hypersensibilité ?`,
+  userInfo_hypersensibilityImpactNotAtAll: `Not at all`,
+  userInfo_hypersensibilityImpactALittle: `A little`,
+  userInfo_hypersensibilityImpactModerately: `Moderately`,
+  userInfo_hypersensibilityImpactALot: `A lot`,
+  userInfo_soundsReactionsLabel: `Y a-t-il des sons en particulier qui déclenchent chez vous des réactions très intenses telles que la colère, le dégoût... ?`,
+  userInfo_soundsReactionsListLabel: `Quels sont les sons qui déclenchent ces réactions ? (séparés par des virgules)`,
+  userInfo_soundsReactionsListPlaceholder: `Bruits de klaxon, grincements de dents`,
+  userInfo_nextForm: `Passer au second formulaire`,
+  noiseTolerance_description: `<p>Pour certains sons, certaines personnes sont moins sensibles, alors que d’autres sont plus sensibles.</p>
+
+      <p>Y a-t-il des sons que vous trouvez particulièrement pénibles ?</p>
+
+      <p>Veuillez indiquer dans quelle mesure vous êtes en accord ou en désaccord avec les déclarations
+        suivantes.</p>`,
+  noiseTolerance_completelyDisagree: `I strongly disagree`,
+  noiseTolerance_disagree: `I disagree`,
+  noiseTolerance_neutral: `I don't agree nor disagree`,
+  noiseTolerance_agree: `I agree`,
+  noiseTolerance_completelyAgree: `I strongly agree`,
+  noiseTolerance_statement1: `Certains sons me dérangent tellement que j’ai du mal à contrôler mes émotions.`,
+  noiseTolerance_statement2: `Les sons déplaisants me donnent l’impression d’être submergé(e).`,
+  noiseTolerance_statement3: `Je deviens anxieux à la simple pensée d’un son désagréable.`,
+  noiseTolerance_statement4: `Je crois que mes réactions aux sons sont exagérées, mais je ne peux pas m’en défaire.`,
+  noiseTolerance_statement5: `Lorsque j’entends des sons déplaisants, je commence à sentir des émotions dans mon corps (par exemple : je transpire, je ressens de la douleur, de la pression, mes muscles se tendent).`,
+  noiseTolerance_statement6: `Je commence à ressentir de la colère dès que je vois un objet/animal/personne qui pourrait produire un son désagréable.`,
+  noiseTolerance_statement7: `Je fais beaucoup d'efforts pour contrôler mes émotions lorsque j'entends un son désagréable.`,
+  noiseTolerance_statement8: `Si je le peux, j'évite de rencontrer certaines personnes à cause des bruits qu'elles produisent.`,
+  noiseTolerance_statement9: `Je trouve certains sons produits par le corps humain insupportables.`,
+  noiseTolerance_statement10: `Je sens que mon état mental s'aggrave si je ne peux pas quitter un endroit où il y a un son désagréable.`,
+  noiseTolerance_statement11: `Je réfléchis souvent à des techniques pour masquer ou noyer les sons désagréables.`,
+  noiseTolerance_statement12: `Certains sons désagréables me mettent immédiatement en colère.`,
+  noiseTolerance_statement13: `Je crains que les sons désagréables puissent avoir un impact sur mon avenir.`,
+  noiseTolerance_statement14: `Lorsque je rencontre d'autres personnes, je suis parfois irrité par la présence de sons désagréables.`,
+  noiseTolerance_dislikeLabel: `Je considère mon aversion à certains sons comme...`,
+  noiseTolerance_dislike1: `Un problème extrême, qui affecte toute ma vie.`,
+  noiseTolerance_dislike2: `Un problème sévère, qui affecte plusieurs aspects de ma qualité de vie.`,
+  noiseTolerance_dislike3: `Un problème modéré, qui affecte quelques aspects de ma qualité de vie.`,
+  noiseTolerance_dislike4: `Un problème léger, qui affecte légèrement ma qualité de vie.`,
+  noiseTolerance_dislike5: `N’est pas un problème, n’affecte pas ma qualité de vie.`,
+  noiseTolerance_configureSound: `Configurer le son pour l'expérience`,
+  soundConfig_description: `<p>Nous allons maintenant ajuster le niveau du son pour qu'il soit présenté à un niveau confortable, ni trop faible, ni trop fort. Vous pouvez régler le volume en déplaçant le curseur ci-après.</p>
+      <p>Vous devez mettre le son de votre appareil au volume maximum.</p>`,
+  common_playSound: `Play the sound`,
+  common_replaySound: `Play the sound again`,
+  common_soundPlaying: `Sound playing...`,
+  soundConfig_startTraining: `Start the training`,
+  soundTraining_description: `<p>Les sons qui suivent sont des sons d'entraînement. Cela vous permet de tester si le volume que vous avez choisi à l'étape précédente vous convient, ou si vous avez besoin de le reconfigurer.</p>
+
+      <p>Une fois ces quelques essais effectués, vous allez pouvoir démarrer l'expérience.</p>`,
+  common_veryPleasant: `Very pleasant`,
+  common_neutral: `Neutral`,
+  common_veryUnpleasant: `Very unpleasant`,
+  soundTraining_nextSound: `Go to the next training sound`,
+  soundTraining_startTests: `Start the tests`,
+  soundTraining_reconfigureSound: `Reconfigure the sound volume`,
+  soundTests_description: `<p>Le test peut maintenant démarrer. Vous devez juger les sons présentés selon une échelle agréable - désagréable. Si le son est très agréable, vous devez cliquer à l’extrême gauche de l'échelle. Si le son est très désagréable, vous devez cliquer à l'extrême droite de l'échelle. Si le son est neutre, vous devez cliquer au milieu de l'échelle. <strong>La plupart des sons sont plus ou moins agréables ou désagréables, vous devez donc utiliser toute la longueur de l'échelle pour répondre.</strong></p>
+
+      <p>Les sons tests sont présentés trois fois dans un ordre aléatoire.</p>
+      <p>Attention : une fois votre réponse enregistrée, il ne sera plus possible de la modifier !</p>
+
+      <p>Bon courage et merci de votre participation !</p>`,
+  soundTests_experimentInProgress: `Experiment in progress...`,
+  soundTests_experimentOver: `Experiment finished!`,
+  soundTests_nextSound: `Go to the next sound`,
+  soundTests_finishExperiment: `End the experiment`,
+  end_description: `Les données ont été automatiquement recueillies, merci !`,
+  end_warningMessage: `Les données de l'expérience n'ont pas pu être recueillies. Veuillez revenir sur cette page ultérieurement (e.g. le lendemain), ou envoyer le texte ci-après directement à la personne en charge de l'expérience :`
+}
+
+export default translations
